f9aba01df74de76b69852a0ae2d920afa6824b79
Karm Binning <karmbinning@gmail.com>
2023-11-02 13:35:14 -0400

    - create JSON teams file

---
+++ js/teams.js
@@ -0,0 +1,18 @@
+function getTeam(){
+	var teams = [
+		{
+			url: "https://a.espncdn.com/combiner/i?img=/i/teamlogos/nba/500/scoreboard/tor.png&scale=crop&cquality=40&location=origin&w=40&h=40",
+			name: "TOR"
+		},
+		{
+			url: "https://a.espncdn.com/combiner/i?img=/i/teamlogos/nba/500/scoreboard/min.png&scale=crop&cquality=40&location=origin&w=40&h=40",
+			name: "MIN"
+		},
+		{
+			url: "https://a.espncdn.com/combiner/i?img=/i/teamlogos/nba/500/scoreboard/mil.png&scale=crop&cquality=40&location=origin&w=40&h=40",
+			name: "MIL"
+		}
+	];
+
+	return teams;
+}
