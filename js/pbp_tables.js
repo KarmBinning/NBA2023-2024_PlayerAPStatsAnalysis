4481a60c98a431d093bfd237c4371c642077e6b0
Karm Binning <karmbinning@gmail.com>
2023-11-02 13:29:27 -0400

    Now go through all team urls to set name

--- js/pbp_tables.js
+++ js/pbp_tables.js
@@ -4,15 +4,34 @@
 var homeTeam = 'TOR', awayTeam = 'MIN';
 
 function replaceLogoWithTeamChars(){
+	var teams = getTeam();
+
 	$('.playByPlay__logo ').each(function(i){
 		var imgSrc = $('img').attr('src');
-		if (imgSrc == "https://a.espncdn.com/combiner/i?img=/i/teamlogos/nba/500/scoreboard/tor.png&scale=crop&cquality=40&location=origin&w=40&h=40")
-			$(this).html("TOR");
-		if (imgSrc == "https://a.espncdn.com/combiner/i?img=/i/teamlogos/nba/500/scoreboard/min.png&scale=crop&cquality=40&location=origin&w=40&h=40")
-			$(this).html("MIN");
-	});
-	
-	
+
+		for (var x=0;x<teams.length;x++)
+			if (teams[x]['url'] == imgSrc)
+				$(this).html(teams[x]['name']);
+	});	
+}
+
+function getTeam(){
+	var teams = [
+		{
+			url: "https://a.espncdn.com/combiner/i?img=/i/teamlogos/nba/500/scoreboard/tor.png&scale=crop&cquality=40&location=origin&w=40&h=40",
+			name: "TOR"
+		},
+		{
+			url: "https://a.espncdn.com/combiner/i?img=/i/teamlogos/nba/500/scoreboard/min.png&scale=crop&cquality=40&location=origin&w=40&h=40",
+			name: "MIN"
+		},
+		{
+			url: "https://a.espncdn.com/combiner/i?img=/i/teamlogos/nba/500/scoreboard/mil.png&scale=crop&cquality=40&location=origin&w=40&h=40",
+			name: "MIL"
+		}
+	];
+
+	return teams;
 }
 
 function addNumCharsToTime(){
