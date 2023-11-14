83fef5453cdba018923d68f371e171e8b3ac897a
Karm Binning <karmbinning@gmail.com>
2023-11-14 06:26:42 -0500

    - add TOR players JSON file

--- js/pbp_players_tor.js
+++ js/pbp_players_tor.js
@@ -1,37 +1,25 @@
-// DESCRIPTION: Roster of Toronto Raptors
+function getPlayersList(){
+	var playersList = 
+	[
+		['Dennis Schröder',         'Point Guard'],
+		['Pascal Siakam',           'Power Forward'],
+		['Gradey Dick',             'Small Forward'],
+		['Scottie Barnes',          'Power Forward'],
+		['OG Anunoby',              'Small Forward'],
+		['Jakob Poeltl',            'Center'],
+		['Gary Trent Jr.',          'Shooting Guard'],
+		['Markquis Nowell',         'Point Guard'],
+		['Garrett Temple',          'Small Forward'],
+		['Javon Freeman-Liberty',   'Shooting Guard'],
+		['Chris Boucher',           'Power Forward'],
+		['Malachi Flynn',           'Point Guard'],
+		['Otto Porter Jr.',         'Power Forward'],
+		['Christian Koloko',        'Center'],
+		['Thaddeus Young',          'Power Forward'],
+		['Precious Achiuwa',        'Power Forward'],
+		['Ron Harper Jr.',          'Small Forward'],
+		['Jalen McDaniels',         'Power Forward']
+	]
 
-// NOTES: Position IDs: PG (1), SG (2), SF (3), PF (4), C (5)
-
-
-/*
-    Data Type
-    ---------
-    string  Player Full Name
-    string  Position
-    //bool    Is Active?
-*/
-var playersList = 
-[
-    ['Dennis Schröder',         'Point Guard'],
-    ['Pascal Siakam',           'Power Forward'],
-    ['Gradey Dick',             'Small Forward'],
-    ['Scottie Barnes',          'Power Forward'],
-    ['OG Anunoby',              'Small Forward'],
-    ['Jakob Poeltl',            'Center'],
-    ['Gary Trent Jr.',          'Shooting Guard'],
-    ['Markquis Nowell',         'Point Guard'],
-    ['Garrett Temple',          'Small Forward'],
-    ['Javon Freeman-Liberty',   'Shooting Guard'],
-    ['Chris Boucher',           'Power Forward'],
-    ['Malachi Flynn',           'Point Guard'],
-    ['Otto Porter Jr.',         'Power Forward'],
-    ['Christian Koloko',        'Center'],
-    ['Thaddeus Young',          'Power Forward'],
-    ['Precious Achiuwa',        'Power Forward'],
-    ['Ron Harper Jr.',          'Small Forward'],
-    ['Jalen McDaniels',         'Power Forward']
-]
-
-
-
-
+	return playersList;
+}
