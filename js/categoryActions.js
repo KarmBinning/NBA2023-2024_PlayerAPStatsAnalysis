8c819ad3fc3baba3a6e0d07c59dd63c0c5cdc343
Karm Binning <karmbinning@gmail.com>
2023-11-22 07:48:42 -0500

    - completed function to get Game ID onload

--- js/categoryActions.js
+++ js/categoryActions.js
@@ -155,6 +155,12 @@ function getPlayersFullName(){
 	return playersFullName;
 }
 
+function getGameId(){
+	var meta_gameId = document.head.querySelector("[property~=game_id][content]").content;
+	log('GAME ID: ' + meta_gameId);
+}
+
 $(document).ready(function(){
 	getEachTeamsPlay('TOR');
+	getGameId();
 });
